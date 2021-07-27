89a9409bf230c2673726894c2904c1d6782fea4b
thatretrodev <thatretrodev@outlook.com>
2021-07-27 17:52:05 -0400

    Add some code

---
+++ config/dev/webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, '../../build'),
+		filename: 'main.js'
+	},
+	plugins: [new HtmlWebpackPlugin({title: "Every Web Framework possible at once."})]
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var test_element = document.createElement("p");
+test_element.innerText = "Hello, World!";
+document.body.appendChild(test_element);
